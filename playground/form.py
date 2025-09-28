50828648336324e551ec1e294b703e978312c93f
parinazomidi <parinazomidi196@gmail.com>
2025-09-28 20:56:17 +0330

    Add mobile responsive and activity page and login register pages

--- playground/form.py
+++ playground/form.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 from .models import Room 
+
 
 class RoomForm(ModelForm): 
     class Meta: 
@@ -8,3 +10,7 @@ class RoomForm(ModelForm):
         #or you can add a list of fields you want to include 
         #fields = ['name', 'description', 'topic']
         exclude = ['host','participants']
+class UserForm(ModelForm): 
+    class Meta: 
+        model = User
+        fields = ['username',  'email']
--- playground/views.py
+++ playground/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room , Topic, Message
-from .form import RoomForm
+from .form import RoomForm, UserForm
 # Create your views here.
 
 
@@ -66,7 +66,7 @@ def home(request):
         Q(description__icontains=q) 
         )
     
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:3]
     room_count = rooms.count()
     room_message= Message.objects.filter(Q(room__topic__name__icontains = q))
     
@@ -117,26 +117,36 @@ def userProfile (request, pk):
 
 @login_required(login_url='login')
 def createRoom(request):
-    print("view called!") 
     form = RoomForm()
+    topics = Topic.objects.all()
     
      
     if request.method == 'POST': 
+        topics_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name= topics_name)
         form = RoomForm(request.POST)
-        if form.is_valid(): 
-            room = form.save(commit=False)
-            room.host =request.user
-            room.save()
-            return redirect('home')
+        
+        Room.objects.create (
+            host= request.user, 
+            topic= topic,
+            name= request.POST.get('name'),
+            description= request.POST.get('description'),
+        )
+        #if form.is_valid(): 
+        #    room = form.save(commit=False)
+        #    room.host =request.user
+        #    room.save()
+        return redirect('home')
     else: 
         form = RoomForm()    
-        context = {'form': form}
+        context = {'form': form, 'topics': topics, 'room': room}
         return render(request, 'playground/room_form.html', context)
     
 @login_required(login_url='login')        
 def updateRoom(request, pk): 
     room = Room.objects.get(id= pk )
     form = RoomForm(instance =room)
+    topics = Topic.objects.all()
 
     if request.user != room.host: 
         return HttpResponse('Your are not allowed here!')
@@ -150,7 +160,7 @@ def updateRoom(request, pk):
     else: 
             form = RoomForm(instance=room)
 
-    context ={'form': form}
+    context ={'form': form, 'topics': topics, 'room': room}
     return render(request, 'playground/room_form.html',context)
 
 @login_required(login_url='login')
@@ -178,6 +188,28 @@ def deleteMessage(request, pk):
         return redirect('home')
     
     return render(request, 'playground/delete.html',{'obj':'message'})   
-    
+
+@login_required(login_url='login')
+def updateUser(request): 
+    user = request.user
+    form = UserForm (instance = user)
+
+    if request.method == 'POST': 
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+        
+    return render (request, 'playground/update-user.html',{'form': form} )
+
+def topicsPage(request): 
+    q = request.GET.get ('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render (request , 'playground/topics.html',{'topics': topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all() 
+    return render (request, 'playground/activity.html', {'room_messages': room_messages}) 
     
 
